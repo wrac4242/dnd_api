bb55644012a22881143fb19ad106603edc768c5c
Wrac4242 <wrac4242@gmail.com>
2022-08-18 17:54:38 +0100

    move all route related code to own file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+	app = express(),
+	port = process.env.PORT || 3000;
+	mongoose = require('mongoose'),
+	routes_creator = require("./routes.js");
 
-const routes_creator = require("./routes.js");
-
-const port = process.env.PORT || 3000;
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_URL || "mongodb://localhost/dnd_db");
 
 routes_creator(app);
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`);
-})
+});
